b3a53f85e27888f724de6ca2bf0cf42b192f251c
junhwong <jun@diosay.com>
2017-02-28 20:56:40 +0800

    构建AST结构

--- src/ast/node.rs
+++ src/ast/node.rs
@@ -1,53 +1,47 @@
-use std::any::Any;
-
-/// 节点类型。
+/// 用于记录节点位于原代码中的位置，展开(lineNo, column)。
 #[derive(Debug)]
-pub enum NodeType{
-    DomNode = 1,
-    DomAttr =2,
-}
+pub struct Pos(i32, i32);
+
+/// 定义的一个语法树的节点集合。
+pub type NodeList<'a> = Vec<Node<'a>>;
 
 /// 定义的一个语法树的节点。
 #[derive(Debug)]
-pub enum  Node{
-    DomNode(i32, i32, Option<Box<Any>>),
-    DomAttr(i32, i32, Box<Any>),
+pub enum Node<'a> {
+    /// 表是一个用于占位的空节点。
+    None,
+    /// 表是一个 DOM 节点，如：div。
+    DomNode {
+        pos: Pos,
+        tag: &'a str,
+        attrs: NodeList<'a>,
+        children: NodeList<'a>,
+    },
+    /// 表示一个 DOM 节点的属性，如： id。
+    DomAttr {
+        pos: Pos,
+        name: &'a str,
+        value: Box<Node<'a>>,
+    }
 }
 
-//impl Node {
-//    fn new(kind: NodeType, line: i32, column: i32, payload: Option) -> Node{
-//        Node{
-//            kind: kind,
-//            payload: None,
-//        }
-//    }
-//}
+#[cfg(test)]
+mod node_tests {
+    #[test]
+    fn test() {
+        let attr = Node::DomAttr {
+            pos: Pos(0, 0),
+            name: "div",
+            value: Box::new(Node::None)
+        };
+        let node = Node::DomNode {
+            pos: Pos(0, 0),
+            tag: "div",
+            attrs: vec![attr],
+            children: vec![],
+        };
 
-//struct DomNode{
-//    attrs: Vec<Box<i32>>,
-//    children: Vec<Node>,
-//}
-struct DomAttr<'a>{
-    name: &'a str,
-    value: Node,
+        println!("{:?}", node);
+    }
 }
 
-#[test]
-fn test(){
-    let inner = Node::DomNode(0,0,None);
-    let node = Node::DomNode(0,10,Some(Box::new(DomAttr{name:"", value: inner})));
-
-    println!("{:?}", node);
-
-    let p = match node{
-        Node::DomNode(_,_,payload) => payload,
-        _=>None,
-    };
-    println!("{:?}", p);
-
-//    let mut dnode= DomNode{attrs:vec![],children:vec![]};
-//
-//    let node = Node::new(NodeType::DomAttr, 0, 1, DomAttr{name: "", value: None});
-//    dnode.attrs.push(node);
-
-}
